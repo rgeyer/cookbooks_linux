8c81fd741c594fd495cf9c6e790a3d7945118911
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-19 17:02:35 -0800

    Something is broken with cyrus-sasl, it seems...

--- cookbooks/cyrus-sasl/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Ryan J. Geyer"
-maintainer_email "me@ryangeyer.com"
-license          "All rights reserved"
-description      "Installs/Configures cyrus-sasl"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
-
-supports "ubuntu"
--- cookbooks/cyrus-sasl/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: cyrus-sasl
-# Recipe:: default
-#
-# Copyright 2011, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
