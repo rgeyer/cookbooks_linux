5a929e70db02db209fc70aeb07dbfee9ab8a7c39
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-20 15:22:18 -0800

    Index is a reserved word?

--- cookbooks/openldap/providers/module.rb
+++ cookbooks/openldap/providers/module.rb
@@ -1,4 +1,5 @@
 action :enable do
+  Chef::Log.info("Enabling OpenLDAP module (#{new_resource.name})")
   unless `ldapsearch -Q -Y EXTERNAL -H ldapi:/// -b cn=config "(&(objectClass=olcModuleList)(olcModuleLoad=#{new_resource.name}))"` =~ /numEntries/
     # TODO: Fairly certain that I can get away without the index here, and anywhere else I use in for cn=config stuff.
     idx = `ldapsearch -Q -Y EXTERNAL -H ldapi:/// -b cn=config "(objectClass=olcModuleList)" | grep numEntries | cut -d' ' -f3`
@@ -8,7 +9,7 @@ action :enable do
       executable "ldapadd"
       source "addModule.ldif.erb"
       source_type :template
-      index idx
+      idx idx
       module_name new_resource.name
     end
   end
