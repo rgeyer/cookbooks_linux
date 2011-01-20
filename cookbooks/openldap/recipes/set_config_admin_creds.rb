e01fbc1f11dbf2f070ac2e792d2fa2dfb177d04e
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-20 13:29:17 -0800

    Let's not forget to actually add the recipe for setting admin creds

---
+++ cookbooks/openldap/recipes/set_config_admin_creds.rb
@@ -0,0 +1,15 @@
+if `ldapsearch -Q -Y EXTERNAL -H ldapi:/// -b "cn=config" "(olcRootPw=*)"` =~ /numEntries/
+  openldap_execute_ldif do
+    executable "ldapadd"
+    source "deleteConfigAdminPassword.ldif"
+    source_type :remote_file
+  end
+end
+
+openldap_execute_ldif do
+  executable "ldapadd"
+  source "setConfigAdminCreds.ldif.erb"
+  source_type :template
+  config_admin_cn node[:openldap][:config_admin_cn]
+  config_admin_password node[:openldap][:config_admin_password]
+end
