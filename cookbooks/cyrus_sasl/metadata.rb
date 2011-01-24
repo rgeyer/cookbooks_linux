c187af562f53c6528d730fe49f582ddc588dc229
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-24 12:59:01 -0800

    Tweaks to cyrus metadata, since Chef on RightScale is not able to parse it

--- cookbooks/cyrus_sasl/metadata.rb
+++ cookbooks/cyrus_sasl/metadata.rb
@@ -6,3 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 supports "ubuntu"
+
+recipe "cyrus_sasl::install", "Installs the cyrus-sasl package"
+recipe "cyrus_sasl::mod_ldap", "Installs the cyrus-sasl ldap package"
