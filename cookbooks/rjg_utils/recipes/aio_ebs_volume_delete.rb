b6f8905e221228d989225c2c29944823a97e0049
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-28 16:35:39 -0800

    Some more prep work before deleting the ebs vol

--- cookbooks/rjg_utils/recipes/aio_ebs_volume_delete.rb
+++ cookbooks/rjg_utils/recipes/aio_ebs_volume_delete.rb
@@ -1,5 +1,12 @@
 include_recipe "aws::default"
 
+# TODO: Is it possible to do this, even with processes running? I suspect not..
+mount node[:rjg_utils][:aio_ebs_mountpoint] do
+  device "/dev/sdi"
+  fstype "xfs"
+  action [:umount, :disable]
+end
+
 aws_ebs_volume "aio_ebs-#{node[:rjg_utils][:rs_instance_uuid]}" do
   aws_access_key node[:aws][:access_key_id]
   aws_secret_access_key node[:aws][:secret_access_key]
