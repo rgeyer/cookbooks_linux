63b33da025ac6460fad67a5b2f6f1b79240f2c0d
Ryan Geyer <rgeyer@its.jnj.com>
2011-02-01 14:08:58 -0800

    oops, forgot to provide a block for only_if

--- cookbooks/rjg_utils/recipes/aio_ebs_volume.rb
+++ cookbooks/rjg_utils/recipes/aio_ebs_volume.rb
@@ -23,7 +23,7 @@ mkfs.xfs -q /dev/sdi
   # the bash code, or make an assumption that if I'm creating a new ebs volume (always the case when no snapshot id
   # is supplied) it needs to be formatted.
   #not_if "mkfs.xfs -N /dev/sdi | grep mkfs.xfs"
-  only_if node[:rjg_utils][:aio_ebs_snapshot_id] == "blank"
+  only_if {node[:rjg_utils][:aio_ebs_snapshot_id] == "blank"}
 end
 
 mount node[:rjg_utils][:aio_ebs_mountpoint] do
