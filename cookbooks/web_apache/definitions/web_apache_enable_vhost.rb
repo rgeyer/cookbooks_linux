c30991353174b8d60e7d72a73515ba5e32f21c79
Ryan Geyer <rgeyer@its.jnj.com>
2011-02-04 09:14:31 -0800

    Forgot to pass the allow_override in

--- cookbooks/web_apache/definitions/web_apache_enable_vhost.rb
+++ cookbooks/web_apache/definitions/web_apache_enable_vhost.rb
@@ -2,6 +2,7 @@ define :web_apache_enable_vhost, :fqdn => nil, :aliases => nil, :allow_override 
   fqdn = params[:fqdn]
   aliases = params[:aliases]
   docroot = "#{node[:web_apache][:content_dir]}/#{fqdn}/htdocs"
+  allow_override = params[:allow_override]
 
   # A workaround for a bug in RightScale's implementation of chef, where exluded optional
   # attributes cause all attributes to be nil.  This allows the user to provide a value
@@ -28,6 +29,7 @@ define :web_apache_enable_vhost, :fqdn => nil, :aliases => nil, :allow_override 
     cookbook "web_apache"
     server_aliases aliases
     docroot docroot
+    allow_override allow_override
   end
 
   right_link_tag "apache2:vhost=#{fqdn}"
