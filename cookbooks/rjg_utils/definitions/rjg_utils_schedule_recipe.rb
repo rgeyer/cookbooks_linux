8eb3324e76c80fff484ca655b067ee3a9faa44ba
Ryan Geyer <me@ryangeyer.com>
2011-03-03 17:37:26 -0800

    Schedule recipe should work on initial boot now, forgot to send the right variable to cron_job

--- cookbooks/rjg_utils/definitions/rjg_utils_schedule_recipe.rb
+++ cookbooks/rjg_utils/definitions/rjg_utils_schedule_recipe.rb
@@ -12,10 +12,10 @@ define :rjg_utils_schedule_recipe, :json_file => nil, :frequency => 'daily', :ac
     daily_recipes -= [{ "recipe" => "#{params[:name]}", "json_file" => "#{params[:json_file]}" }]
   end
   case params[:frequency]
-    when 'hourly' then node[:rjg_utils][:hourly_recipes] = daily_recipes.uniq
-    when 'daily' then node[:rjg_utils][:daily_recipes] = daily_recipes.uniq
-    when 'weekly' then node[:rjg_utils][:weekly_recipes] = daily_recipes.uniq
-    when 'monthly' then node[:rjg_utils][:monthly_recipes] = daily_recipes.uniq
+    when 'hourly' then node_attribute = node[:rjg_utils][:hourly_recipes] = daily_recipes.uniq
+    when 'daily' then node_attribute = node[:rjg_utils][:daily_recipes] = daily_recipes.uniq
+    when 'weekly' then node_attribute = node[:rjg_utils][:weekly_recipes] = daily_recipes.uniq
+    when 'monthly' then node_attribute = node[:rjg_utils][:monthly_recipes] = daily_recipes.uniq
   end
 
   cron_job "recipes_#{params[:frequency]}" do
