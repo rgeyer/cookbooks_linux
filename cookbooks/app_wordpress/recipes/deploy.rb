1ebcfbfefd3fe0cb264e74c80e6c449ee4538f0d
Ryan Geyer <me@ryangeyer.com>
2011-03-03 20:46:10 -0800

    Debugz yo

--- cookbooks/app_wordpress/recipes/deploy.rb
+++ cookbooks/app_wordpress/recipes/deploy.rb
@@ -1,13 +1,17 @@
 # TODO: So this sorta assumes that an instance runs either nginx or apache2, but not both
 is_apache2 = `which apache2` != nil
+content_dir = is_apache2 ? node[:web_apache][:content_dir] : node[:nginx][:content_dir]
+webserver = is_apache2 ? "apache2" : "nginx"
 
 Chef::Log.info("Is Apache2 was (#{is_apache2})")
+Chef::Log.info("Content dir was (#{content_dir})")
+Chef::Log.info("Webserver was (#{webserver})")
 
 app_wordpress_site "Deploy Wordpress for a vhost" do
   fqdn node[:app_wordpress][:vhost_fqdn]
   aliases node[:app_wordpress][:vhost_aliases]
   db_pass node[:app_wordpress][:db_pass]
-  content_dir is_apache2 ? node[:web_apache][:content_dir] : node[:nginx][:content_dir]
-  webserver is_apache2 ? "apache2" : "nginx"
+  content_dir content_dir
+  webserver webserver
   action :install
 end
