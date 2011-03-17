b6cf171c282851120829dcff15510fda424100d0
Ryan Geyer <me@ryangeyer.com>
2011-03-16 18:28:59 -0700

    Fixed attribute names in metadata

--- cookbooks/rightgrid/metadata.rb
+++ cookbooks/rightgrid/metadata.rb
@@ -7,13 +7,13 @@ version          "0.0.1"
 
 recipe "rightgrid::default", "Installs the specified RightGrid application and gets it rolling"
 
-attribute "rightgrid/access_key_id",
+attribute "rightgrid/aws_access_key_id",
   :display_name => "Access Key Id",
   :description => "This is an Amazon credential. Log in to your AWS account at aws.amazon.com to retrieve you access identifiers. Ex: 1JHQQ4KVEVM02KVEVM02",
   :recipes => ["rightgrid::default"],
   :required => "required"
 
-attribute "rightgrid/secret_access_key",
+attribute "rightgrid/aws_secret_access_key",
   :display_name => "Secret Access Key",
   :description => "This is an Amazon credential. Log in to your AWS account at aws.amazon.com to retrieve your access identifiers. Ex: XVdxPgOM4auGcMlPz61IZGotpr9LzzI07tT8s2Ws",
   :recipes => ["rightgrid::default"],
