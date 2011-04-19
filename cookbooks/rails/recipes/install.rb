05e6b6449fa892946e3c4deca778d33afc52d129
Ryan Geyer <me@ryangeyer.com>
2011-04-19 16:31:48 -0700

    including the rubygems recipe, perhaps needlessly

--- cookbooks/rails/recipes/install.rb
+++ cookbooks/rails/recipes/install.rb
@@ -15,6 +15,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+include_recipe "rubygems::default"
+
 gem_package "rails" do
   version node[:rails][:version] if node[:rails][:version]
   gem_binary "/usr/local/bin/gem" if ::File.exists?("/usr/local/bin/ree-version")
