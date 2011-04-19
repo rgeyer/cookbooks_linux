c330bd14574a16a3201589a1f59eeadb882e6ad2
Ryan Geyer <me@ryangeyer.com>
2011-04-19 14:46:45 -0700

    Added compile_gemset to rvm

--- cookbooks/rvm/metadata.rb
+++ cookbooks/rvm/metadata.rb
@@ -8,6 +8,7 @@ version          "0.0.1"
 supports "ubuntu"
 
 recipe "rvm::default", "Installs Ruby Version Manager (RVM)"
+recipe "rvm::compile_gemset", "Creates a binary version of the specified gemset for both i386 and x86_64 arch's"
 
 attribute "rvm/install_path",
   :display_name => "RVM Installation Path",
@@ -15,3 +16,39 @@ attribute "rvm/install_path",
   :required => "optional",
   :default => "/opt/rvm",
   :recipes => ["rvm::default"]
+
+attribute "rvm/compile_gemset/ruby",
+  :display_name => "RVM Ruby Name",
+  :description => "The full RVM version. To find a list run `rvm list known`.  I.E. ruby-1.8.7-head",
+  :required => "required",
+  :recipes => ["rvm::compile_gemset"]
+
+attribute "rvm/compile_gemset/gemset_file",
+  :display_name => "RVM gemset export file",
+  :description => "An RVM exported gemset file that can be found inside of the defined s3_bucket",
+  :required => "required",
+  :recipes => ["rvm::compile_gemset"]
+
+attribute "rvm/compile_gemset/gemset_name",
+  :display_name => "RVM gemset name",
+  :description => "The name of the gemset to create and upload to s3",
+  :required => "required",
+  :recipes => ["rvm::compile_gemset"]
+
+attribute "rvm/compile_gemset/s3_bucket",
+  :display_name => "RVM gemset S3 bucket",
+  :description => "S3 bucket name",
+  :required => "required",
+  :recipes => ["rvm::compile_gemset"]
+
+attribute "aws/access_key_id",
+  :display_name => "Access Key Id",
+  :description => "This is an Amazon credential. Log in to your AWS account at aws.amazon.com to retrieve you access identifiers. Ex: 1JHQQ4KVEVM02KVEVM02",
+  :recipes => ["rvm::compile_gemset"],
+  :required => "required"
+
+attribute "aws/secret_access_key",
+  :display_name => "Secret Access Key",
+  :description => "This is an Amazon credential. Log in to your AWS account at aws.amazon.com to retrieve your access identifiers. Ex: XVdxPgOM4auGcMlPz61IZGotpr9LzzI07tT8s2Ws",
+  :recipes => ["rvm::compile_gemset"],
+  :required => "required"
---
+++ cookbooks/rvm/recipes/compile_gemset.rb
@@ -0,0 +1,74 @@
+#
+# Cookbook Name:: rvm
+# Recipe:: compile_gemset
+#
+#  Copyright 2011 Ryan J. Geyer
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+gemset_file = "/tmp/gemset.gems"
+gemset_dir = ::File.join(node[:rvm][:install_path], "gems", "#{node[:rvm][:compile_gemset][:ruby]}@compile_me")
+
+include_recipe "rvm::default"
+
+rjg_aws_s3 "Download gemset file" do
+  access_key_id node[:aws][:access_key_id]
+  secret_access_key node[:aws][:secret_access_key]
+  s3_bucket node[:rvm][:compile_gemset][:s3_bucket]
+  s3_file node[:rvm][:compile_gemset][:gemset_file]
+  file_path gemset_file
+  action :get
+end
+
+bash "Create and upload gem binaries (i386 arch)" do
+  code <<-EOF
+rvm_archflags="-arch i386" CFLAGS="-arch i386" LDFLAGS="-arch i386" rvm install #{node[:rvm][:compile_gemset][:ruby]}
+rvm --default use #{node[:rvm][:compile_gemset][:ruby]}
+rvm --create #{node[:rvm][:compile_gemset][:ruby]}@compile_me
+rvm gemset import #{gemset_file}
+cd #{gemset_dir}
+tar -cf /tmp/#{node[:rvm][:compile_gemset][:gemset_name]}-i386.tar *
+gzip /tmp/#{node[:rvm][:compile_gemset][:gemset_name]}-i386.tar
+  EOF
+end
+
+bash "Create and upload gem binaries (x86_64 arch)" do
+  code <<-EOF
+rvm remove #{node[:rvm][:compile_gemset][:ruby]}
+rm -rf #{gemset_dir}
+rvm_archflags="-arch x86_64" CFLAGS="-arch x86_64" LDFLAGS="-arch x86_64" rvm install #{node[:rvm][:compile_gemset][:ruby]}
+rvm --default use #{node[:rvm][:compile_gemset][:ruby]}
+rvm --create #{node[:rvm][:compile_gemset][:ruby]}@compile_me
+rvm gemset import #{gemset_file}
+cd #{gemset_dir}
+tar -cf /tmp/#{node[:rvm][:compile_gemset][:gemset_name]}-amd64.tar *
+gzip /tmp/#{node[:rvm][:compile_gemset][:gemset_name]}-amd64.tar
+  EOF
+end
+
+rjg_aws_s3 "Upload i386 file" do
+  access_key_id node[:aws][:access_key_id]
+  secret_access_key node[:aws][:secret_access_key]
+  s3_bucket node[:rvm][:compile_gemset][:s3_bucket]
+  s3_file node[:rvm][:compile_gemset][:gemset_file]
+  file_path "/tmp/#{node[:rvm][:compile_gemset][:gemset_name]}-i386.tar.gz"
+  action :put
+end
+
+rjg_aws_s3 "Upload x86_64 file" do
+  access_key_id node[:aws][:access_key_id]
+  secret_access_key node[:aws][:secret_access_key]
+  s3_bucket node[:rvm][:compile_gemset][:s3_bucket]
+  s3_file node[:rvm][:compile_gemset][:gemset_file]
+  file_path "/tmp/#{node[:rvm][:compile_gemset][:gemset_name]}-amd64.tar.gz"
+  action :put
+end
