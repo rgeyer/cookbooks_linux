3ed8f7658087e63f5f4ca46c8d5b361c0fe1d01e
Ryan Geyer <me@ryangeyer.com>
2011-05-05 10:00:29 -0700

    debugz

--- cookbooks/rjg_utils/recipes/default.rb
+++ cookbooks/rjg_utils/recipes/default.rb
@@ -17,35 +17,39 @@
 #  code "env"
 #end
 
-bash "Load rvm scirpt & show me the gems" do
-  code <<-EOF
-rvm_script="#{node[:rvm][:install_path]}/scripts/rvm"
-echo "Testing for RVM using $rvm_scripts"
-if [ ! -f $rvm_script ]
-then
-  echo "No RVM installation found, not loading RVM environment"
-  exit 0
-fi
-
-if [[ -s "$rvm_script" ]] ; then
-  echo "Found a default RVM environment, loading it now"
-  source "$rvm_script"
-else
-  echo "No default RVM environment found, can not continue.  Try setting one with rvm --default"
-  exit 0
-fi
-
-echo "The default RVM environment is $rvm_ruby_string"
-
-echo "Gem List coming right up"
-
-gem list
-
-echo "RVM Info coming right up"
-
-rvm info
-EOF
+bash "Check for RVM" do
+  code 'echo "RVM check for latest is $rvm_ruby_string"'
 end
+
+#bash "Load rvm scirpt & show me the gems" do
+#  code <<-EOF
+#rvm_script="#{node[:rvm][:install_path]}/scripts/rvm"
+#echo "Testing for RVM using $rvm_scripts"
+#if [ ! -f $rvm_script ]
+#then
+#  echo "No RVM installation found, not loading RVM environment"
+#  exit 0
+#fi
+#
+#if [[ -s "$rvm_script" ]] ; then
+#  echo "Found a default RVM environment, loading it now"
+#  source "$rvm_script"
+#else
+#  echo "No default RVM environment found, can not continue.  Try setting one with rvm --default"
+#  exit 0
+#fi
+#
+#echo "The default RVM environment is $rvm_ruby_string"
+#
+#echo "Gem List coming right up"
+#
+#gem list
+#
+#echo "RVM Info coming right up"
+#
+#rvm info
+#EOF
+#end
 
 #bash "Here comes env again" do
 #  code "env"
