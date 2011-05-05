94f55eae6579231da87eaa28810343f069b3d3d8
Ryan Geyer <me@ryangeyer.com>
2011-05-05 09:33:05 -0700

    debugz

--- cookbooks/rjg_utils/recipes/default.rb
+++ cookbooks/rjg_utils/recipes/default.rb
@@ -17,25 +17,33 @@
 #  code "env"
 #end
 
-bash "Load bashrc & show me the gems" do
+bash "Load rvm scirpt & show me the gems" do
   code <<-EOF
-rvm_bin="#{node[:rvm][:install_path]}/bin/rvm"
-echo "Testing for RVM using $rvm_bin"
-if [ ! -f $rvm_bin ]
+rvm_script="#{node[:rvm][:install_path]}/scripts/rvm"
+echo "Testing for RVM using $rvm_scripts"
+if [ ! -f $rvm_script ]
 then
   echo "No RVM installation found, not loading RVM environment"
   exit 0
 fi
 
-if [[ -s "#{node[:rvm][:install_path]}/environments/default" ]] ; then
+if [[ -s "$rvm_script" ]] ; then
   echo "Found a default RVM environment, loading it now"
-  source "#{node[:rvm][:install_path]}/environments/default"
+  source "$rvm_script"
 else
   echo "No default RVM environment found, can not continue.  Try setting one with rvm --default"
   exit 0
 fi
 
-env
+echo "The default RVM environment is $rvm_ruby_string"
+
+echo "Gem List coming right up"
+
+gem list
+
+echo "RVM Info coming right up"
+
+rvm info
 EOF
 end
 
