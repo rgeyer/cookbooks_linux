b7333fc6012754909115b1dc1ccf492ad5870fde
Ryan Geyer <me@ryangeyer.com>
2011-05-12 12:13:32 -0700

    Nuke all existing ruby binaries and libs, replace everything with RVM installed ruby

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -15,6 +15,11 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# TODO: Test erasing of standard ruby & replacing w/ RVM
+# This will effectively "erase" any ruby presence, and make the currently selected RVM environment
+# the "system" ruby
+
+bindir=::File.join(node[:rvm][:install_path], 'bin')
 node[:rvm][:bin_path] = ::File.join(node[:rvm][:install_path], "bin", "rvm")
 
 bash "Download the RVM install script" do
@@ -31,4 +36,21 @@ bash "Install RVM for all users" do
 #{node[:rvm][:bin_path]} reload
   EOF
   not_if { ::File.exists?(node[:rvm][:install_path]) }
+end
+
+# Erase all existence of "standard" ruby 1.8 and replace it with the RVM installed/default ruby
+if node[:platform] == "ubuntu"
+  %w{libopenssl-ruby1.8 libreadline-ruby1.8 libruby1.8 libshadow-ruby1.8 ruby ruby1.8 ruby1.8-dev}.each do |p|
+    package p do
+      action :remove
+    end
+  end
+
+  bash "Symlink RVM binaries to /usr/bin" do
+    code "for bin in `ls #{bindir}`; do ln -sf #{bindir}/$bin /usr/bin/$bin; done;"
+    creates "/usr/bin/ruby"
+    action :run
+  end
+else
+  Chef::Log.info("Your platform (#{node[:platform]}) is not supported by this recipe!")
 end
