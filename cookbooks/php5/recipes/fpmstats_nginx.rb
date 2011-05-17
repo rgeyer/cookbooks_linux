a4895b0c4aa0c8c1bae412da022167231f77335b
Ryan Geyer <me@ryangeyer.com>
2011-05-16 21:04:59 -0700

    Forgot to give the nginx fpm stats config the right syntax for a unix socket

--- cookbooks/php5/recipes/fpmstats_nginx.rb
+++ cookbooks/php5/recipes/fpmstats_nginx.rb
@@ -9,7 +9,7 @@ node[:rs_utils][:process_list] += " php5-fpm" unless node[:rs_utils][:process_li
 nginx_conf = ::File.join(node[:nginx][:dir], "sites-available", "#{node[:hostname]}.d", "php5-fpm-stats.conf")
 nginx_collectd_conf = ::File.join(node[:rs_utils][:collectd_plugin_dir], "php5-fpm.conf")
 
-listen_str = node[:php5_fpm][:listen] == "socket" ? node[:php5_fpm][:listen_socket] : "#{node[:php5_fpm][:listen_ip]}:#{node[:php5_fpm][:listen_port]}"
+listen_str = node[:php5_fpm][:listen] == "socket" ? "unix:#{node[:php5_fpm][:listen_socket]}" : "#{node[:php5_fpm][:listen_ip]}:#{node[:php5_fpm][:listen_port]}"
 
 # This is necessary for collectd's curl_json plugin, but apparently not installed already *shrug*
 package "libyajl1"
