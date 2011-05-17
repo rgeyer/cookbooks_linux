2e5af27d7f5c85c2a2027fa015a3b9db0135bdc0
Ryan Geyer <me@ryangeyer.com>
2011-05-16 17:42:58 -0700

    removed the default 'blank'

--- cookbooks/rjg_utils/attributes/default.rb
+++ cookbooks/rjg_utils/attributes/default.rb
@@ -3,6 +3,5 @@ default[:rjg_utils][:daily_recipes] = []
 default[:rjg_utils][:weekly_recipes] = []
 default[:rjg_utils][:monthly_recipes] = []
 
-default[:rjg_utils][:aio_ebs_snapshot_id] = "blank"
 default[:rjg_utils][:aio_ebs_mountpoint] = "/srv"
 default[:rjg_utils][:aio_ebs_snapshots_to_keep] = "7"
