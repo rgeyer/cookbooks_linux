cbe4cf802e38b1d383f9da664af7c4090b35cb49
Ryan J. Geyer <me@ryangeyer.com>
2011-08-31 14:26:16 -0700

    A healthier check for node[:ec2]

--- cookbooks/php5/attributes/default.rb
+++ cookbooks/php5/attributes/default.rb
@@ -4,7 +4,7 @@ default[:php5_fpm][:listen_socket]  = "/var/run/php5-fpm.sock"
 default[:php5_fpm][:listen_ip]      = "127.0.0.1"
 default[:php5_fpm][:listen_port]    = "9000"
 
-if node.ec2
+if attribute?(:ec2)
   default[:php5][:fpm_log_dir]  = "/mnt/log/php5-fpm"
 else
   default[:php5][:fpm_log_dir]  = "/var/log/php5-fpm"
--- cookbooks/unicorn/attributes/default.rb
+++ cookbooks/unicorn/attributes/default.rb
@@ -2,7 +2,7 @@ default[:unicorn][:version] = nil
 # TODO: Copied from OpsCode sample application recipe, maybe we want something a bit more scientific?
 default[:unicorn][:worker_processes] = [node[:cpu][:total].to_i * 4, 8].min
 
-if node.ec2
+if attribute?(:ec2)
   default[:unicorn][:log_path] = "/mnt/log/unicorn"
 else
   default[:unicorn][:log_path] = "/var/log/unicorn"
