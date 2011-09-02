b3f16ae34c77093654a69e18efb784d7efc969ed
Ryan J. Geyer <me@ryangeyer.com>
2011-09-02 15:13:49 -0700

    Switch to ruby to launch the instance and upload to it

--- cookbooks/rax_rebundler/recipes/launch.rb
+++ cookbooks/rax_rebundler/recipes/launch.rb
@@ -15,12 +15,29 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-bash "Launch a new Rackspace instance from the specified image id" do
-  cwd node[:rax_rebundler][:path]
-  code <<-EOH
-  export RACKSPACE_ACCOUNT="#{node[:rax_rebundler][:rax_username]}"
-  export RACKSPACE_API_TOKEN="#{node[:rax_rebundler][:rax_api_token]}"
-  #{::File.join("bin","launch")} #{node[:rax_rebundler][:instance_name]} #{node[:rax_rebundler][:image_id]}
-  #{::File.join("bin","upload")} #{node[:rax_rebundler][:image_type]}
-EOH
+ruby_block "Launch a new Rackspace instance from the specified image id" do
+  block do
+    require "pp"
+    require "yaml"
+
+    ENV["RACKSPACE_ACCOUNT"] = node[:rax_rebundler][:rax_username]
+    ENV["RACKSPACE_API_TOKEN"] = node[:rax_rebundler][:rax_api_token]
+
+    launch_bin = ::File.join(node[:rax_rebundler][:path],"bin","launch")
+    upload_bin = ::File.join(node[:rax_rebundler][:path],"bin","upload")
+
+    yaml_result = `#{launch_bin} #{node[:rax_rebundler][:instance_name]} #{node[:rax_rebundler][:image_id]} yaml`
+    hash_result = YAML::load(yaml_result)
+
+    `#{upload_bin} #{hash_result["server"]["addresses"]["public"]} #{node[:rax_rebundler][:image_type]}`
+
+    pp hash_result
+#  cwd node[:rax_rebundler][:path]
+#  code <<-EOH
+#  export RACKSPACE_ACCOUNT="#{node[:rax_rebundler][:rax_username]}"
+#  export RACKSPACE_API_TOKEN="#{node[:rax_rebundler][:rax_api_token]}"
+#  #{::File.join("bin","launch")} #{node[:rax_rebundler][:instance_name]} #{node[:rax_rebundler][:image_id]}
+#  #{::File.join("bin","upload")} #{node[:rax_rebundler][:image_type]}
+#EOH
+  end
 end
