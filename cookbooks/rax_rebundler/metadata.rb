dc2bd33f618f7b24efde2aaac3c2968cd5adbf3a
Ryan J. Geyer <me@ryangeyer.com>
2011-09-02 10:10:43 -0700

    Added launch to the mix

--- cookbooks/rax_rebundler/metadata.rb
+++ cookbooks/rax_rebundler/metadata.rb
@@ -12,6 +12,24 @@ supports "centos"
 recipe "rax_rebundler::default", "Installs rackspace_rebundler from git and installs dependent gems"
 recipe "rax_rebundler::launch", "Launches a specified image ID in the RAX account referenced by the provided credentials"
 
+attribute "rax_rebundler/rax_username",
+  :display_name => "Rackspace Username",
+  :description => "Your Rackspace Dashboard and API username",
+  :required => "required",
+  :recipes => ["rax_rebundler::launch"]
+
+attribute "rax_rebundler/rax_api_token",
+  :display_name => "Rackspace API Token",
+  :description => "Your Rackspace API Token",
+  :required => "required",
+  :recipes => ["rax_rebundler::launch"]
+
+attribute "rax_rebundler/image_id",
+  :display_name => "Rackspace Image Id",
+  :description => "The unique resource ID of the rackspace image you want to launch in your account",
+  :required => "required",
+  :recipes => ["rax_rebundler::launch"]
+
 attribute "rvm/install_path",
   :display_name => "RVM Installation Path",
   :description => "The full path where RVM will be installed. I.E. /opt/rvm",
---
+++ cookbooks/rax_rebundler/recipes/launch.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: rax_rebundler
+# Recipe:: launch
+#
+#  Copyright 2011 Ryan J. Geyer
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+bash "Launch a new Rackspace instance from the specified image id" do
+  cwd node[:rax_rebundler][:path]
+  code <<-EOH
+  export RACKSPACE_ACCOUNT="#{node[:rax_rebundler][:rax_username]}"
+  export RACKSPACE_API_TOKEN="#{node[:rax_rebundler][:rax_api_token]}"
+  #{::File.join("bin","launch")} #{node[:rax_rebundler][:image_id]}
+EOH
+end
