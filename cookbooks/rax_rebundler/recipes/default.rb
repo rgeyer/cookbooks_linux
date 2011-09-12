546cd17ff020f4a7d7ffdc2eb73d83fd7bb3d6a7
Ryan J. Geyer <me@ryangeyer.com>
2011-09-12 13:55:31 -0700

    include rs_utils::setup_monitoring so that there is a collectd service resource to restart

--- cookbooks/rax_rebundler/recipes/default.rb
+++ cookbooks/rax_rebundler/recipes/default.rb
@@ -15,6 +15,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+include_recipe "rs_utils::setup_monitoring"
 include_recipe "rvm::default"
 
 # Load the filecount plugin in the main collectd config file
