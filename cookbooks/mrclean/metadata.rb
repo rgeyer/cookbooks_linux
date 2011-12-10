895b1200e9d5a03c5ea98bfd93531fb6d9b8ce16
Ryan J. Geyer <me@ryangeyer.com>
2011-12-09 17:57:10 -0800

    Initial commit of mrclean cookbook

---
+++ cookbooks/mrclean/metadata.rb
@@ -0,0 +1,112 @@
+maintainer       "Ryan J. Geyer"
+maintainer_email "me@ryangeyer.com"
+license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
+description      "Installs and Configures the Python tool (MrClean)"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+%w{ubuntu centos}.each do |os|
+  supports os
+end
+
+%w{scheduler rs_utils}.each do |dep|
+  depends dep
+end
+
+recipe "mrclean::install_from_svn", "Fetches the MrClean sourcecode from SVN and sets up a configuration"
+recipe "mrclean::do_mrclean", "Executes the MrClean python script"
+recipe "mrclean::do_enable_continuous_cleanup", "Schedules the MrClean python script to run daily"
+recipe "mrclean::do_disable_continuous_cleanup", "Disables the daily scheduled execution of the MrClean python script"
+
+attribute "mrclean/svn_repo",
+  :display_name => "MrClean SVN Repo URI",
+  :description => "The URI to use when checking out the MrClean source code",
+  :required => "required",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/svn_username",
+  :display_name => "MrClean SVN Repo username",
+  :description => "The username to authenticate with SVN to check out the MrClean source code",
+  :required => "optional",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/svn_password",
+  :display_name => "MrClean SVN Repo password",
+  :description => "The password to authenticate with SVN to check out the MrClean source code",
+  :required => "optional",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/api_username",
+  :display_name => "MrClean RightScale API username",
+  :description => "The username to authenticate with the RightScale API",
+  :required => "optional",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/api_password",
+  :display_name => "MrClean RightScale API password",
+  :description => "The password to authenticate with the RightScale API",
+  :required => "optional",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/install_dir",
+  :display_name => "MrClean Install Directory",
+  :description => "The directory where MrClean will be installed, and run from",
+  :required => "optional",
+  :default => "/home/mrclean",
+  :recipes => ["mrclean::do_mrclean","mrclean::install_from_svn"]
+
+attribute "mrclean/accounts",
+  :display_name => "MrClean RightScale Account List",
+  :description => "A list of RightScale accounts which MrClean will clean up",
+  :required => "required",
+  :type => "array",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/deployments",
+  :display_name => "MrClean RightScale Deployments List",
+  :description => "A list of RightScale deployments which MrClean will not delete from the accounts listed in mrclean/accounts",
+  :required => "required",
+  :type => "array",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/arrays",
+  :display_name => "MrClean RightScale Server Arrays List",
+  :description => "A list of RightScale server arrays which MrClean will not delete from the accounts listed in mrclean/accounts",
+  :required => "required",
+  :type => "array",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/templates",
+  :display_name => "MrClean RightScale Server Templates List",
+  :description => "A list of RightScale server templates which MrClean will not delete from the accounts listed in mrclean/accounts",
+  :required => "required",
+  :type => "array",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/servers",
+  :display_name => "MrClean RightScale Servers List",
+  :description => "A list of RightScale servers which MrClean will not delete from the accounts listed in mrclean/accounts",
+  :required => "required",
+  :type => "array",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/macros",
+  :display_name => "MrClean RightScale Macros List",
+  :description => "A list of RightScale macros which MrClean will not delete from the accounts listed in mrclean/accounts",
+  :required => "required",
+  :type => "array",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/credentials",
+  :display_name => "MrClean RightScale Credential List",
+  :description => "A list of RightScale credentials which MrClean will not delete from the accounts listed in mrclean/accounts",
+  :required => "required",
+  :type => "array",
+  :recipes => ["mrclean::install_from_svn"]
+
+attribute "mrclean/security_groups",
+  :display_name => "MrClean RightScale Security Groups List",
+  :description => "A list of RightScale security groups which MrClean will not delete from the accounts listed in mrclean/accounts",
+  :required => "required",
+  :type => "array",
+  :recipes => ["mrclean::install_from_svn"]
---
+++ cookbooks/mrclean/recipes/install_from_svn.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: mrclean
+# Recipe:: default
+#
+# Copyright 2011, Ryan J. Geyer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+rs_utils_marker :begin
+
+directory node[:mrclean][:install_dir] do
+  recursive true
+  action :create
+end
+
+subversion node[:mrclean][:install_dir] do
+  repository node[:mrclean][:svn_repo]
+  if node[:mrclean][:svn_username]
+    svn_username node[:mrclean][:svn_username]
+  end
+  if node[:mrclean][:svn_password]
+    svn_password node[:mrclean][:svn_password]
+  end
+  revision "master"
+  action :sync
+end
+
+template ::File.join(node[:mrclean][:install_dir], 'mrclean.py') do
+  local true
+  backup false
+  source ::File.join(node[:mrclean][:install_dir], 'rightsync-example.py.erb')
+end
+
+rs_utils_marker :end
