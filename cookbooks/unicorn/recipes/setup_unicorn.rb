f815454efc9d511ef1c59afdcb97a027fad34984
Ryan J. Geyer <me@ryangeyer.com>
2012-01-22 16:46:22 -0800

    Make unicorn play a little nicer with rvm

--- cookbooks/unicorn/recipes/setup_unicorn.rb
+++ cookbooks/unicorn/recipes/setup_unicorn.rb
@@ -33,39 +33,4 @@ template "/etc/logrotate.d/unicorn" do
   source "logrotate.d.erb"
 end
 
-#default_ruby = `#{node[:rvm][:bin_path]} list default string`.strip
-#Chef::Log.info("Creating unicorn init wrapper for rvm.  Using rvm binary #{node[:rvm][:bin_path]}.  Using ruby #{default_ruby}")
-bash "Create a unicorn_rails rvm wrapper if necessary" do
-  code <<-EOF
-rvm_script="#{node[:rvm][:install_path]}/scripts/rvm"
-echo "Testing for RVM using $rvm_script"
-if [ ! -f $rvm_script ]
-then
-  echo "No RVM installation found, not loading RVM environment"
-  exit 0
-fi
-
-if [[ -s "$rvm_script" ]] ; then
-  echo "Found a default RVM environment, loading it now"
-  source "$rvm_script"
-else
-  echo "No default RVM environment found, can not continue.  Try setting one with rvm --default"
-  exit 0
-fi
-
-unicorn_wrapper="#{node[:rvm][:install_path]}/bin/init_unicorn_rails"
-echo "Testing for RVM unicorn_rails wrapper using $unicorn_wrapper"
-if [ ! -f $unicorn_wrapper ]
-then
-  echo "Creating RVM wrapper for unicorn_rails in gemset $rvm_ruby_string"
-  rvm wrapper $rvm_ruby_string init unicorn_rails
-else
-  echo "RVM wrapper for unicorn_rails already exists, skipping"
-  exit 0
-fi
-
-exit 0
-  EOF
-end
-
 rs_utils_marker :end
