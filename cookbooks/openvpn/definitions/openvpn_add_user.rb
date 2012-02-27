b6f081c3213dbf0a7101a6256c2d138f92e47d45
Ryan J. Geyer <me@ryangeyer.com>
2012-02-27 13:03:56 -0800

    Initial commit of RS'ized openvpn cookbook

---
+++ cookbooks/openvpn/definitions/openvpn_add_user.rb
@@ -0,0 +1,49 @@
+# Copyright 2012, Ryan J. Geyer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+define :openvpn_add_user do
+  execute "generate-openvpn-#{params[:name]}" do
+    command "./pkitool #{params[:name]}"
+    cwd "/etc/openvpn/easy-rsa"
+    environment(
+      'EASY_RSA' => '/etc/openvpn/easy-rsa',
+      'KEY_CONFIG' => '/etc/openvpn/easy-rsa/openssl.cnf',
+      'KEY_DIR' => node["openvpn"]["key_dir"],
+      'CA_EXPIRE' => node["openvpn"]["key"]["ca_expire"].to_s,
+      'KEY_EXPIRE' => node["openvpn"]["key"]["expire"].to_s,
+      'KEY_SIZE' => node["openvpn"]["key"]["size"].to_s,
+      'KEY_COUNTRY' => node["openvpn"]["key"]["country"],
+      'KEY_PROVINCE' => node["openvpn"]["key"]["province"],
+      'KEY_CITY' => node["openvpn"]["key"]["city"],
+      'KEY_ORG' => node["openvpn"]["key"]["org"],
+      'KEY_EMAIL' => node["openvpn"]["key"]["email"]
+    )
+    not_if { ::File.exists?("#{node["openvpn"]["key_dir"]}/#{params[:name]}.crt") }
+  end
+
+  %w{ conf ovpn }.each do |ext|
+    template "#{node["openvpn"]["key_dir"]}/#{params[:name]}.#{ext}" do
+      source "client.conf.erb"
+      variables :username => params[:name]
+    end
+  end
+
+  execute "create-openvpn-tar-#{params[:name]}" do
+    cwd node["openvpn"]["key_dir"]
+    command <<-EOH
+      tar zcf #{params[:name]}.tar.gz ca.crt #{params[:name]}.crt #{params[:name]}.key #{params[:name]}.conf #{params[:name]}.ovpn
+    EOH
+    not_if { ::File.exists?("#{node["openvpn"]["key_dir"]}/#{params[:name]}.tar.gz") }
+  end
+end
