70c01392c1ccf5f5333deeb5f08765c88140fd00
Ryan J. Geyer <me@ryangeyer.com>
2012-02-28 12:53:00 -0800

    added a default recipe for openvpn so the cookbook gets downloaded during a boot runlist on RS

--- cookbooks/openvpn/metadata.rb
+++ cookbooks/openvpn/metadata.rb
@@ -5,7 +5,8 @@ description       "Installs and configures openvpn and includes rake tasks for m
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.99.2"
 
-recipe "openvpn::default", "Installs and configures openvpn"
+recipe "openvpn::default", "Does nothing"
+recipe "openvpn::setup_server", "Installs and configures openvpn"
 recipe "openvpn::users", "Sets up openvpn cert/configs for users data bag items"
 
 %w{ redhat centos fedora ubuntu debian }.each do |os|
@@ -22,31 +23,31 @@ attribute "openvpn/local",
   :display_name => "OpenVPN Local",
   :description => "Local interface (ip) to listen on",
   :default => "env:PUBLIC_IP",
-  :recipes => ["openvpn::default"]
+  :recipes => ["openvpn::setup_server"]
 
 attribute "openvpn/proto",
   :display_name => "OpenVPN Protocol",
   :description => "UDP or TCP",
   :default => "udp",
-  :recipes => ["openvpn::default"]
+  :recipes => ["openvpn::setup_server"]
 
 attribute "openvpn/type",
   :display_name => "OpenVPN Type",
   :description => "Server or server-bridge",
   :default => "server",
-  :recipes => ["openvpn::default"]
+  :recipes => ["openvpn::setup_server"]
 
 attribute "openvpn/subnet",
   :display_name => "OpenVPN Subnet",
   :description => "Subnet to hand out to clients",
   :default => "10.8.0.0",
-  :recipes => ["openvpn::default"]
+  :recipes => ["openvpn::setup_server"]
 
 attribute "openvpn/netmask",
   :display_name => "OpenVPN Netmask",
   :description => "Netmask for clients",
   :default => "255.255.0.0",
-  :recipes => ["openvpn::default"]
+  :recipes => ["openvpn::setup_server"]
 
 attribute "openvpn/users",
   :display_name => "OpenVPN Users",
