71e8b7e69159cd5a1a0255ef837ea5b3910cf0b7
Ryan J. Geyer <me@ryangeyer.com>
2012-02-28 11:50:07 -0800

    Added setup_single_node_management_server recipe

--- cookbooks/cloudstack/metadata.rb
+++ cookbooks/cloudstack/metadata.rb
@@ -10,20 +10,48 @@ supports "centos"
 depends "rs_utils"
 depends "sys_firewall"
 
+recommends "db"
+recommends "db_mysql"
+recommends "openvpn"
+
 recipe "cloudstack::install_cloudstack", "Installs the CloudStack binary files used for setting up the CloudStack agent and management server"
-recipe "cloudstack::setup_management_server", "Sets up a single node CloudStack management server"
+recipe "cloudstack::setup_management_server", "Sets up the CloudStack management server software"
+recipe "cloudstack::setup_single_node_management_server", "Sets up a single node CloudStack management server"
 
 attribute "cloudstack/csmanage/dbuser",
   :display_name => "CloudStack Management Database Username",
   :description => "The database username for the CloudStack Management Server DB connection",
-  :required => "required"
+  :required => "required",
+  :recipes => ["cloudstack::setup_management_server","cloudstack::setup_single_node_management_server"]
   
 attribute "cloudstack/csmanage/dbpass",
   :display_name => "CloudStack Management Database Password",
   :description => "The database password for the CloudStack Management Server DB connection",
-  :required => "required"
+  :required => "required",
+  :recipes => ["cloudstack::setup_management_server","cloudstack::setup_single_node_management_server"]
 
 attribute "cloudstack/csmanage/dbhost",
-    :display_name => "CloudStack Management Database Hostname",
-    :description => "The database hostname for the CloudStack Management Server DB connection",
-    :required => "required"
+  :display_name => "CloudStack Management Database Hostname",
+  :description => "The database hostname for the CloudStack Management Server DB connection",
+  :required => "required",
+  :recipes => ["cloudstack::setup_management_server","cloudstack::setup_single_node_management_server"]
+
+attribute "cloudstack/csmanage/vpn/listen_ip",
+  :display_name => "CloudStack Management VPN ListenIP",
+  :description => "The ip address for OpenVPN to listen on",
+  :required => "required",
+  :recipes => ["cloudstack::setup_single_node_management_server"]
+
+attribute "cloustack/csmanage/vpn/subnet",
+  :display_name => "CloudStack Management VPN Subnet",
+  :description => "The ip subnet for OpenVPN to use",
+  :required => "optional",
+  :default => "176.16.1.0",
+  :recipes => ["cloudstack::setup_single_node_management_server"]
+
+attribute "cloustack/csmanage/vpn/netmask",
+  :display_name => "CloudStack Management VPN Netmask",
+  :description => "The ip netmask for OpenVPN to use",
+  :required => "optional",
+  :default => "255.255.255.0",
+  :recipes => ["cloudstack::setup_single_node_management_server"]
---
+++ cookbooks/cloudstack/recipes/setup_single_node_management_server.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: cloudstack
+# Recipe:: setup_management_server
+#
+#  Copyright 2012 Ryan J. Geyer
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+rs_utils_marker :begin
+
+node[:openvpn][:local]    = node[:cloudstack][:csmanage][:vpn][:local]
+node[:openvpn][:netmask]  = node[:cloudstack][:csmanage][:vpn][:netmask]
+node[:openvpn][:subnet]   = node[:cloudstack][:csmanage][:vpn][:subnet]
+node[:openvpn][:protocol] = "udp"
+node[:openvpn][:type]     = "server"
+node[:openvpn][:users]    = ["remote"]
+
+include_recipe "cloudstack::setup_management_server"
+include_recipe "openvpn::default"
+include_recipe "openvpn::users"
+
+rs_utils_marker :end
