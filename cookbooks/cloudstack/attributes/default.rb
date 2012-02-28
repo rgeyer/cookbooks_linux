795d25533647c7ebe99ecc5e6d5319c79ad99363
Ryan J. Geyer <me@ryangeyer.com>
2012-02-28 12:05:20 -0800

    Header typo, and actually providing default attributes

--- cookbooks/cloudstack/attributes/default.rb
+++ cookbooks/cloudstack/attributes/default.rb
@@ -1,2 +1,5 @@
 default[:cloudstack][:package_url] = "http://sourceforge.net/projects/cloudstack/files/Cloudstack%202.2/2.2.13/CloudStack-2.2.13-1-rhel5.tar.gz"
 default[:cloudstack][:install_dir] = "/opt/cloudstack"
+
+default[:cloudstack][:csmanage][:vpn][:subnet] = "176.16.1.0"
+default[:cloudstack][:csmanage][:vpn][:netmask] = "255.255.255.0"
--- cookbooks/cloudstack/recipes/setup_single_node_management_server.rb
+++ cookbooks/cloudstack/recipes/setup_single_node_management_server.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: cloudstack
-# Recipe:: setup_management_server
+# Recipe:: setup_single_node_management_server
 #
 #  Copyright 2012 Ryan J. Geyer
 #  Licensed under the Apache License, Version 2.0 (the "License");
