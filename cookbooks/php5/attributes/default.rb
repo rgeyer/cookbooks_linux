b637ef7df46a923f72d33d9962402230f1b55f0f
Ryan J. Geyer <me@ryangeyer.com>
2012-07-02 20:41:46 -0700

    Removing smarty from default php packages, there are better ways

--- cookbooks/php5/attributes/default.rb
+++ cookbooks/php5/attributes/default.rb
@@ -5,11 +5,11 @@ default[:php5_fpm][:listen_ip]      = "127.0.0.1"
 default[:php5_fpm][:listen_port]    = "9000"
 
 case node[:platform]
-  when "centos"
+  when "centos", "rhel"
     case
       when node[:platform_version].to_f > 5.4
         default[:php5][:packages_remove] = %w{php php-cli php-common}
-        default[:php5][:packages] = %w{php53u php53u-cli php-Smarty php53u-pear}
+        default[:php5][:packages] = %w{php53u php53u-cli php53u-pear}
         default[:php5][:package_prefix] = "php53u-"
         default[:php5][:conf_d_path] = "/etc/php.d"
         default[:php5_fpm][:service_name] = "php-fpm"
@@ -19,7 +19,7 @@ case node[:platform]
     case node[:platform_version]
       when "10.04"
         default[:php5][:packages_remove] = []
-        default[:php5][:packages] = %w{php5-cgi php5-cli smarty php-pear}
+        default[:php5][:packages] = %w{php5-cgi php5-cli php-pear}
         default[:php5][:package_prefix] = "php5-"
         default[:php5][:conf_d_path] = "/etc/php5/conf.d"
         default[:php5_fpm][:service_name] = "php5-fpm"
