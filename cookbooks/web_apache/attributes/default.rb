59d9087b90226fa1c47199f838da11cf0f9db8b3
Ryan J. Geyer <me@ryangeyer.com>
2012-07-07 17:27:08 -0700

    Remove web_apache due to conflicts

--- cookbooks/web_apache/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default[:web_apache][:content_dir] = "/mnt/www"
-default[:web_apache][:aliases] = []
--- cookbooks/web_apache/recipes/enable_site_vhost.rb
+++
@@ -1,4 +0,0 @@
-web_apache_enable_vhost "Vhost" do
-  fqdn node[:web_apache][:vhost_fqdn]
-  aliases node[:web_apache][:aliases]
-end
