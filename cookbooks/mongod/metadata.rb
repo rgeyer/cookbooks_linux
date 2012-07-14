b54ea2bb3e6b6ed5f87392c44579055bcf58a020
Ryan J. Geyer <me@ryangeyer.com>
2012-07-13 17:08:51 -0700

    Use an actual array instead of a string

--- cookbooks/mongod/metadata.rb
+++ cookbooks/mongod/metadata.rb
@@ -5,11 +5,11 @@ description      "Installs/Configures mongodb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
-%{rightscale block_device}.each do |dep|
+%w{rightscale block_device}.each do |dep|
   depends dep
 end
 
-%{ubuntu debian rhel centos}.each do |sup|
+%w{ubuntu debian rhel centos}.each do |sup|
   supports sup
 end
 
